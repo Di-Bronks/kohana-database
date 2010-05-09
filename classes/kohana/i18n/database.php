6e59e1e06e4147eaebad3f249078796e98b61b6e
David Pommer <pommer@code8.de>
2010-05-09 17:20:42 +0200

    Initial commit of Kohana_I18n_Database

---
+++ classes/kohana/i18n/database.php
@@ -0,0 +1,100 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Database-based i18n reader.
+ *
+ * @package    Kohana
+ * @category   I18n
+ * @author     Kohana Team
+ * @copyright  (c) 2010 Kohana Team
+ * @license    http://kohanaphp.com/license
+ */
+class Kohana_I18n_Database extends Kohana_I18n_Reader {
+
+	protected $_database_instance = 'default';
+
+	protected $_database_table = 'i18n';
+
+	public function __construct(array $config = NULL)
+	{
+		if (isset($config['instance']))
+		{
+			$this->_database_instance = $config['instance'];
+		}
+
+		if (isset($config['table']))
+		{
+			$this->_database_table = $config['table'];
+		}
+
+		parent::__construct();
+	}
+
+	/**
+	 * Query the i18n table for all values for this lang
+	 *
+	 * @param   string  i18n lang
+	 * @return  $this   clone of the current object
+	 */
+	public function load($lang, array $messages = NULL)
+	{
+		if ($messages === NULL)
+		{
+			// Split the language: language, region, locale, etc
+			$parts = explode('-', $lang);
+
+			$table = array();
+
+			do
+			{
+				// Create a locale for this set of parts
+				$locale = implode('-', $parts);
+
+				$query = DB::select('key', 'value')
+						->from($this->_database_table)
+						->where('locale', '=', $locale)
+						//->limit(1)
+						->execute($this->_database_instance);
+
+				if (count($query) > 0)
+				{
+					$table += $query->as_array('key', 'value');
+				}
+
+				// Remove the last part
+				array_pop($parts);
+			}
+			while ($parts);
+
+			if ( ! empty($table))
+			{
+				$messages = $table;
+			}
+		}
+
+		return parent::load($lang, $messages);
+	}
+
+	/**
+	 * Overload setting offsets to insert or update the database values as 
+	 * changes occur.
+	 *
+	 * @param   string   array key
+	 * @param   mixed    new value
+	 * @return  mixed
+	 */
+	public function offsetGet($key)
+	{
+		/*if ( ! $this->offsetExists($key))
+		{
+			// Insert a new value
+			DB::insert($this->_database_table, array('locale', 'key', 'value'))
+				->values(array($this->_lang, $key, $key))
+				->execute($this->_database_instance);
+
+			return $key;
+		}*/
+
+		return parent::offsetGet($key);
+	}
+
+} // End Kohana_I18n_Database
